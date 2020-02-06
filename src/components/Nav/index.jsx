2c9641c71d9afac80ecde3a70ecaf8c3ee105dcf
EmirVelazquez <emirleovelazquez@gmail.com>
2020-02-05 23:12:25 -0600

    removed uncessary files that come installed with create-react, added bootstrap cdn and google font cdn, added img asset, added a navbar react component

---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./style.css";
+import gargoyleLogo from "./gargoylesSmall.png";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <a className="navbar-brand" href="/">
+        <img
+          src={gargoyleLogo}
+          class="gargoyleLogo"
+          alt="Gargoyles Show Logo"
+        ></img>
+      </a>
+      <div className="nav navbar-nav ml-auto">
+        <p className="clickStart">Begin by clicking an image!</p>
+      </div>
+      <div className="nav navbar-nav ml-auto">
+        <p className="">Current Score:</p>
+        <p className="">Top Score:</p>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
