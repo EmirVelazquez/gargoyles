03bb22548819ee478f41c100d320c9ce492c9bd3
EmirVelazquez <emirleovelazquez@gmail.com>
2020-02-09 16:04:14 -0600

    Updates

---
+++ precache-manifest.2ff5f0469514cbdffe978e395f7ece44.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe31ebe4162d51ee4e672f7155e3d62e",
+    "url": "/gargoyles/index.html"
+  },
+  {
+    "revision": "14a6e5ce00a4d5431117",
+    "url": "/gargoyles/static/css/main.b9f54c87.chunk.css"
+  },
+  {
+    "revision": "6f209fa1f1ba8feb6771",
+    "url": "/gargoyles/static/js/2.fb1169f7.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/gargoyles/static/js/2.fb1169f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a6e5ce00a4d5431117",
+    "url": "/gargoyles/static/js/main.95662810.chunk.js"
+  },
+  {
+    "revision": "3bc633ac847f4ac121d2",
+    "url": "/gargoyles/static/js/runtime-main.bbc57e8c.js"
+  },
+  {
+    "revision": "7484f488f68d777c3fc509fe3760679d",
+    "url": "/gargoyles/static/media/Bricks.7484f488.png"
+  },
+  {
+    "revision": "05875715e00d85c8469f37b195fe2f1f",
+    "url": "/gargoyles/static/media/Gargoyles.05875715.png"
+  },
+  {
+    "revision": "3af2427853aca8857715f86968a18fe1",
+    "url": "/gargoyles/static/media/gargoylesSmall.3af24278.png"
+  },
+  {
+    "revision": "763384325c47ec25876d0481794741a7",
+    "url": "/gargoyles/static/media/goliathSticker.76338432.png"
+  }
+]);
